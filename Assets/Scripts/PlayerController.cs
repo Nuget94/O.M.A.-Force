2d8c9803e230f0d349ede2bdfcc937cb45a1d83b
FJOPTOP\Fjonan <fjonan@fjonan.com>
2017-01-20 23:47:57 +0100

    first level, player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,48 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
+	public float 				moveSpeed 		= 6;							// how fast the player moves
+
+	private Vector3 			velocity;
+	private float				currentSpeed;
+	private new Rigidbody 		rigidbody;
+	private new SpriteRenderer	spriteRenderer;
+
+
 	void Start () {
+		rigidbody 		= GetComponent<Rigidbody>();
+		spriteRenderer	= GetComponent<SpriteRenderer> ();
+	}
+
+	void Update () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	void FixedUpdate() {		
+		// Move the player around
+		velocity 	 = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical")).normalized * moveSpeed;
+		rigidbody.MovePosition (rigidbody.position + velocity * Time.fixedDeltaTime);
+
+		if (Input.GetAxisRaw ("Horizontal") == -1){
+			spriteRenderer.flipX = true;
+		} else if ( Input.GetAxisRaw ("Horizontal") == 1 ) {
+			spriteRenderer.flipX = false;
+		};
+	}
+
+	void OnTriggerEnter(Collider trigger) {
+		
+	}
+
+	void OnTriggerExit(Collider trigger) {
+		
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
 		
 	}
 }

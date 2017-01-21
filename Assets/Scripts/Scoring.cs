fb157546a454a73c80c5390d57f6a55401e1129f
Thorsten Schillo <thorsten.schillo@googlemail.com>
2017-01-21 19:37:48 +0100

    moved waypoints to own folder

---
+++ Assets/Scripts/Scoring.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoring : MonoBehaviour
+{
+    public long score = 0;
+
+    public long scoreForLeavingGuest = 100;
+
+    public void guestLeft()
+    {
+        score += scoreForLeavingGuest;
+    }
+}

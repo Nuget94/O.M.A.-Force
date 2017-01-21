a5024a68f7b95f59be14a15c25c2fc07a86672ab
Dominique martin <dominique-martin@web.de>
2017-01-21 19:05:38 +0100

    [edit] Decoration Movement

--- Assets/Scripts/MoveDecoration.cs
+++ Assets/Scripts/MoveDecoration.cs
@@ -1,16 +1,43 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveDecoration : MonoBehaviour {
+public class MoveDecoration : MonoBehaviour
+{
+    public float jumpForce = 10;
+    public float bpm = 120;
+
+    private static float time;
+
 
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start ()
+    {
+        SetTime();
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+	    if (time < Time.realtimeSinceStartup)
+	    {
+            SetTime();
+	        Rigidbody2D[] bodies = GetComponentsInChildren<Rigidbody2D>();
+	        foreach (Rigidbody2D body in bodies)
+            {
+	            body.AddForce(new Vector2(0, jumpForce));
+            }
+            
+	    }
+
 	}
+
+    void SetTime()
+    {
+        time = Time.realtimeSinceStartup + 1 / (bpm / 60);
+    }   
+    
+
 }
+
